8346539e4c1d41dc4f5a107d691bd55014694877
Himangshu Saikia <arctic.quasar@gmail.com>
2013-08-28 00:05:45 +0200

    navigation

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-	The ladder is not associated in any way with the offical Wesnoth forum or its moderators/developers. <br><br>
+	This ladder is not associated in any way with the offical Wesnoth forum or its moderators/developers. <br><br>
 <a href=#>Terms of Use</a> |
 <a href=#>Privacy Policy</a> |
 <a href=#>Contact</a>
